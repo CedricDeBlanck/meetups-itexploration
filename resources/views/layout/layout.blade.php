444fcbb65ab06d4cda1db57b42e2613b276fdb59
CedricDeBlanck <cedrdebl1@student.arteveldehs.be>
2021-03-19 19:11:10 +0100

    fixed

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,3 +1,6 @@
+<?php
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'){ $protocol = "https://".$_SERVER['HTTP_HOST']; } else{ $protocol='http://'.$_SERVER['HTTP_HOST']; }
+?>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
